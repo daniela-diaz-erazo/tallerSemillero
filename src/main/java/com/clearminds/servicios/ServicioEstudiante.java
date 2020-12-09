3d22b6670c6f541cb529b1a7218a637b19eed642
Daniela Diaz <ampa.dde2106@gmail.com>
2020-12-09 13:36:18 -0500

    Test insertando en la base de datos, y verificando

--- src/main/java/com/clearminds/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/servicios/ServicioEstudiante.java
@@ -1,5 +1,7 @@
 package com.clearminds.servicios;
 
+import java.sql.*;
+
 import com.clearminds.ddi.dtos.Estudiante;
 import com.clearminds.ddi.excepciones.BDDException;
 
@@ -8,7 +10,21 @@ public class ServicioEstudiante extends ServicioBase {
 	public void insertarEstudiante(Estudiante estudiante) throws BDDException {
 		abrirConexion();
 		System.out.println("Insertando Estudiante: " + estudiante.toString());
-		cerrarConexion();
+		
+		Statement stmt = null;
+
+		try {
+			stmt = conexion.createStatement();
+			String sql = "insert into estudiantes(nombre,apellido) values('"+ estudiante.getNombre()+"','"
+					+estudiante.getApellido()+"')";
+			System.out.println("Script: " + sql);
+			stmt.executeUpdate(sql);
+			cerrarConexion();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new BDDException("Error al insertar estudiante");
+		}
+
 	}
 
 }
