5a88186e29ae48b53e082f7e4931793ee758c9d3
Daniela Diaz <ampa.dde2106@gmail.com>
2020-12-09 14:02:33 -0500

    Test Actualizar - actualizando un registro en la base de datos

--- src/main/java/com/clearminds/ddi/bdd/test/TestActualizar.java
+++ src/main/java/com/clearminds/ddi/bdd/test/TestActualizar.java
@@ -4,12 +4,11 @@ import com.clearminds.ddi.dtos.Estudiante;
 import com.clearminds.ddi.excepciones.BDDException;
 import com.clearminds.servicios.ServicioEstudiante;
 
-public class TestServicio {
+public class TestActualizar {
 	public static void main(String[] args) {
-
 		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
 		try {
-			srvEstudiante.insertarEstudiante(new Estudiante("Marco", "Chavez"));
+			srvEstudiante.actualizarEstudiante(new Estudiante("Pedro", "Naranja", 2));
 		} catch (BDDException e) {
 			e.printStackTrace();
 			e.getMessage();
--- src/main/java/com/clearminds/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/servicios/ServicioEstudiante.java
@@ -10,13 +10,13 @@ public class ServicioEstudiante extends ServicioBase {
 	public void insertarEstudiante(Estudiante estudiante) throws BDDException {
 		abrirConexion();
 		System.out.println("Insertando Estudiante: " + estudiante.toString());
-		
+
 		Statement stmt = null;
 
 		try {
 			stmt = conexion.createStatement();
-			String sql = "insert into estudiantes(nombre,apellido) values('"+ estudiante.getNombre()+"','"
-					+estudiante.getApellido()+"')";
+			String sql = "insert into estudiantes(nombre,apellido) values('" + estudiante.getNombre() + "','"
+					+ estudiante.getApellido() + "')";
 			System.out.println("Script: " + sql);
 			stmt.executeUpdate(sql);
 			cerrarConexion();
@@ -25,6 +25,25 @@ public class ServicioEstudiante extends ServicioBase {
 			throw new BDDException("Error al insertar estudiante");
 		}
 
+	}
+
+	public void actualizarEstudiante(Estudiante estudiante) throws BDDException {
+		abrirConexion();
+		System.out.println("Actualizando Estudiante: " + estudiante.toString());
+
+		Statement stmt = null;
+
+		try {
+			stmt = conexion.createStatement();
+			String sql = "update estudiantes set nombre='" + estudiante.getNombre() + "', apellido='" + estudiante.getApellido()
+					+ "' where id=" + estudiante.getId();
+			System.out.println("Script: " + sql);
+			stmt.executeUpdate(sql);
+			cerrarConexion();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new BDDException("Error al actualizar estudiante");
+		}
 	}
 
 }
