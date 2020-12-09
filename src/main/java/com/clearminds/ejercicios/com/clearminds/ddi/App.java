0bbaa4fd9ae92bee6f1a2ffe62f035d64e7c410f
Daniela Diaz <ampa.dde2106@gmail.com>
2020-12-09 09:20:46 -0500

    Versión Inicial

---
+++ src/main/java/com/clearminds/ejercicios/com/clearminds/ddi/App.java
@@ -0,0 +1,13 @@
+package com.clearminds.ejercicios.com.clearminds.ddi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
